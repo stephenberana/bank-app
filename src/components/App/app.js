8cabba17ca02c436c72f3b9e136702219c47a464
stephenberana <stephenberana@gmail.com>
2021-08-23 21:12:03 +0800

    Pushes pre-deployment build of bank app

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,30 +1,29 @@
-import React from 'react';
-import LoginForm from '../Login/loginform';
-import Dashboard from '../Dashboard/dashboard';
-import RegisterNow from '../Login/register-link';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React from "react";
+import LoginForm from "../Login/loginform";
+import Dashboard from "../Dashboard/dashboard";
+import RegisterNow from "../Login/register-link";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends React.Component {
-    render() {
-        return (
-        <Router>
-        <div className="app">    
-            <Switch>
-                <Route 
-                path="/bank-app">
-                    <LoginForm />
-                </Route>
-                <Route path="/register-link">
-                    <RegisterNow />
-                </Route>
-                <Route path="/dashboard">
-                    <Dashboard />
-                </Route>
-            </Switch>
+  render() {
+    return (
+      <Router>
+        <div className="app">
+          <Switch>
+            <Route path="/main">
+              <LoginForm />
+            </Route>
+            <Route path="/register-link">
+              <RegisterNow />
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+          </Switch>
         </div>
-        </Router>
-        );
-    }
+      </Router>
+    );
+  }
 }
 
 export default App;
